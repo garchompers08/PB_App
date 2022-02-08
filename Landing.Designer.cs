2cd84ebb8c35347d51282439e40f55e9b65683f5
PATRICK HOWARD YANGO <phlyango@mymail.mapua.edu.ph>
2022-02-08 17:59:48 +0800

    Updated forms

---
+++ Landing.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PB_App
+{
+    partial class Landing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Landing));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Login = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.Signup = new System.Windows.Forms.Button();
+            this.About = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(355, 532);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.Login.FlatAppearance.BorderSize = 0;
+            this.Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Login.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login.ForeColor = System.Drawing.Color.White;
+            this.Login.Location = new System.Drawing.Point(105, 228);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(144, 52);
+            this.Login.TabIndex = 1;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = false;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Signup
+            // 
+            this.Signup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.Signup.FlatAppearance.BorderSize = 0;
+            this.Signup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Signup.Font = new System.Drawing.Font("Poppins", 12F);
+            this.Signup.ForeColor = System.Drawing.Color.White;
+            this.Signup.Location = new System.Drawing.Point(105, 286);
+            this.Signup.Name = "Signup";
+            this.Signup.Size = new System.Drawing.Size(144, 52);
+            this.Signup.TabIndex = 2;
+            this.Signup.Text = "Signup";
+            this.Signup.UseVisualStyleBackColor = false;
+            this.Signup.Click += new System.EventHandler(this.Signup_Click);
+            // 
+            // About
+            // 
+            this.About.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.About.FlatAppearance.BorderSize = 0;
+            this.About.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.About.Font = new System.Drawing.Font("Poppins", 12F);
+            this.About.ForeColor = System.Drawing.Color.White;
+            this.About.Location = new System.Drawing.Point(105, 344);
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(144, 52);
+            this.About.TabIndex = 3;
+            this.About.Text = "About";
+            this.About.UseVisualStyleBackColor = false;
+            this.About.Click += new System.EventHandler(this.About_Click);
+            // 
+            // Landing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(194)))), ((int)(((byte)(53)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(353, 521);
+            this.Controls.Add(this.About);
+            this.Controls.Add(this.Signup);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Landing";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button Signup;
+        private System.Windows.Forms.Button About;
+    }
+}
+
--- MyQuizzes.cs
+++ MyQuizzes.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PB_App
 {
-    public partial class Form1 : Form
+    public partial class MyQuizzes : Form
     {
-        public Form1()
+        public MyQuizzes()
         {
             InitializeComponent();
         }
